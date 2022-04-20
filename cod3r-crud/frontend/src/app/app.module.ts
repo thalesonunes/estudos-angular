1e631e1570ddfc3741a4365c77dc5dbd04090a77
Thales Nunes <thales.o.nunes@gmail.com>
2022-04-19 22:44:06 -0300

    Atualizar produto finalizado!

--- cod3r-crud/frontend/src/app/app.module.ts
+++ cod3r-crud/frontend/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { MatSortModule } from '@angular/material/sort';
 // Tratando para que os dados de moeda funcionem em pt-br
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 registerLocaleData(localePt);
 
 @NgModule({
@@ -45,7 +46,8 @@ registerLocaleData(localePt);
     ProductCrudComponent,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductRead2Component,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
